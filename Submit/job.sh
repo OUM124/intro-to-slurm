bf616c94a5d5529a666ea8eb2675d91c2cfc2a54
oumaima mouimi <oumaima.mouimi@frontend02.cm.cluster>
2023-11-27 10:44:22 +0000

    Assignment submission

--- Submit/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=my_job2
-#SBATCH --output=out.txt
-#SBATCH --error=error.txt
-#SBATCH --mem-per-cpu=4GB
-#SBATCH --cpu-per-task=4
-
-echo "The first job with --mem and  --cpu-per-task"
--- Submit/my_job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=my_job2
-#SBATCH --output=out.txt
-#SBATCH --error=error.txt
-
-echo "Hello,Slurm!" -> out.txt
